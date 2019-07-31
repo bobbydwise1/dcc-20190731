77a6a08fdd2289f35efa05ddb120d970db0ac26c
Robert Lee <bobbydwise1@hotmail.com>
2019-07-31 15:45:48 -0700

    Rename variables.

--- scripts.js
+++ scripts.js
@@ -6,24 +6,28 @@ A builder is looking to build a row of N houses that can be of K different color
 Given an N by K matrix where the nth row and kth column represents the cost to build the nth house with kth color, return the minimum cost which achieves this goal.
 */
 
-const makeRandomPrice = (lowerRange,upperRange) => {
+const makeRandom = (lowerRange,upperRange) => {
   if (lowerRange >= upperRange) {return 0};
   return Math.round(Math.random()*(upperRange-lowerRange)+lowerRange);
 }
 
-const makeArray = (n,k) => {
+const makeMatrix = (n,k) => {
   finalArray = [];
   for (y=0; y<k; y++) {
     tempArray = [];
     for (x=0; x<n; x++) {
-      tempArray.push(makeRandomPrice(10000,20000));
+      tempArray.push(makeRandom(10000,20000));
     };
     finalArray.push(tempArray);
   };
   return finalArray;
 };
 
-console.log(makeArray(3,3))
+console.log(makeMatrix(3,3))
+
+const findLowest = (yourMatrix) => {
+  
+}
 
 $(document).ready(function() {
   $('#output-section-1').text(1);
